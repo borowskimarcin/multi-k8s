bbe4bcd5244787fae02d970200e1851aa3eebabf
Marcin Borowski <marbor112@gmail.com>
2020-06-06 10:46:36 +0200

    Pushing images fixed

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ docker build -t marbor1/multi-client:latest marbor1/multi-client:$SHA -f ./clien
 docker build -t marbor1/multi-server:latest marbor1/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t marbor1/multi-worker:latest marbor1/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push marbor1/multi-client:latest
-docker push marbor1/multi-client:latest
 docker push marbor1/multi-server:latest
+docker push marbor1/multi-worker:latest
 
+docker push marbor1/multi-client:$SHA
 docker push marbor1/multi-server:$SHA
-docker push marbor1/multi-worker:$SHA
 docker push marbor1/multi-worker:$SHA
 
 kubectl apply -f k8s
